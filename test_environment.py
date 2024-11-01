4a1c9020e1b191ac0ba0e320936dc814c2094861
alvinastr <119154719+alvinastr@users.noreply.github.com>
2024-11-02 01:22:57 +0700

    first commit

---
+++ test_environment.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+print("Matplotlib version:", plt.__version__)
+print("TensorFlow version:", tf.__version__)
+
+print("Conda environment setup complete!")
